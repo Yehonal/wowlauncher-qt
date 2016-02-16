dafcb9b85fe44e43e5465a8c7aa3160fc0adc0df
Yehonal <hw.2@hotmail.it>
2016-02-16 15:05:05 +0100

    bare bone version of WoWLauncher written in QT

---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox *msgBox = new QMessageBox(this);
+    msgBox->setText("Test");
+    msgBox->setWindowModality(Qt::NonModal);
+    msgBox->setInformativeText("Do you want to save your changes?");
+    msgBox->setStandardButtons(QMessageBox::Save | QMessageBox::Discard |
+                               QMessageBox::Cancel);
+    msgBox->setDefaultButton(QMessageBox::Save);
+    int ret = msgBox->exec();
+}
---
+++ wowlauncher.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-06-26T00:46:27
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+QT += webkitwidgets
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = wowlauncher
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
